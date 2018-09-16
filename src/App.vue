cf15efce540e9eff3d604e8d91ec253333d2ed70
Richard Boardman <rickschmoo@gmail.com>
2018-09-15 21:28:12 -0700

    Simple hover

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorldRick test="123"/>
+    <HelloWorldRick test="123" message="hover 123"/>
     <HelloWorld msg="Welcome to Your Vue.js App 123"/>
   </div>
 </template>
--- src/components/HelloWorldRick.vue
+++ src/components/HelloWorldRick.vue
@@ -1,20 +1,27 @@
 <template>
   <div class="helloworldrick">
     <h1>{{ test }}</h1>
-    <p>{{ test }}</p>
+    <p>{{ test }} {{ message }}</p>
     <ul>
       <li>1</li>
       <li>2</li>
       <li>3</li>
     </ul>
+    <div id="app-2">
+      <span v-bind:title="message">
+        Hover your mouse over me for a few seconds
+        to see my dynamically bound title!
+      </span>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld 123',
+  name: 'HelloWorld123',
   props: {
-    test: String
+    test: String,
+    message: String
   }
 }
 </script>
